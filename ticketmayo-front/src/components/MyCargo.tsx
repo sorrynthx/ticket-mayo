d6014f27bc68e0fd14d54f1e4bbf0f7e66d48b8b
sorrynthx <sorrynthx@gmail.com>
2022-05-04 17:52:49 +0900

    2022-05-04 티켓마요 프로젝트 배경색 설정 - 핑크 계열

--- ticketmayo-front/src/components/MyCargo.tsx
+++ ticketmayo-front/src/components/MyCargo.tsx
@@ -1,6 +1,14 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
+`;
+
 function MyCargo() {
     return (
-        <div>MyCargo</div>
+        <Container>MyCargo</Container>
     );
 };
 
--- ticketmayo-front/src/components/Nav/BottomNav.tsx
+++ ticketmayo-front/src/components/Nav/BottomNav.tsx
@@ -1,48 +1,62 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faHome, faUserCircle } from '@fortawesome/free-solid-svg-icons';
-import { Nav, NavItem} from 'reactstrap';
-import { NavLink } from 'react-router-dom';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faTicket, faHome, faTicketSimple, faCircleUser} from '@fortawesome/free-solid-svg-icons';
+import {Nav, NavItem} from 'reactstrap';
+import {NavLink} from 'react-router-dom';
+import styled from 'styled-components';
 
-const tabs = [{
-    route: "/",
-    icon: faHome,
-    label: ""
-  },{
-    route: "/yes24",
-    icon: faSearch,
-    label: ""
-  },{
-    route: "/Interpark",
-    icon: faUserCircle,
-    label: ""
-  },{
-    route: "/myCargo",
-    icon: faUserCircle,
-    label: ""
-  }];
+const tabs = [
+    {
+        route: "/",
+        icon: faHome,
+        label: ""
+    }, {
+        route: "/yes24",
+        icon: faTicket,
+        label: ""
+    }, {
+        route: "/Interpark",
+        icon: faTicketSimple,
+        label: ""
+    }, {
+        route: "/myCargo",
+        icon: faCircleUser,
+        label: ""
+    }
+];
 
+const BottomNavDiv = styled.div `
+  background-color: #FB6F92;
+  
+`;
 
+const BottomNavText = styled.div `
+  color: #fff;
+`
 
 function BottomNav() {
     return (
-      <nav className="navbar fixed-bottom navbar-light d-block d-lg-none bottom-tab-nav" role="navigation">
-        <Nav className="w-100">
-          <div className=" d-flex flex-row justify-content-around w-100">
-            {
-              tabs.map((tab, index) =>(
-                <NavItem key={`tab-${index}`}>
-                  <NavLink to={tab.route} className="nav-link">
-                    <div className="row justify-content-center align-items-center">
-                      <FontAwesomeIcon size="lg" icon={tab.icon}/>
-                      <div>{tab.label}</div>
-                    </div>
-                  </NavLink>
-                </NavItem>
-              ))
-            }
-          </div>
-        </Nav>
-      </nav>
+
+        <BottomNavDiv
+            className="navbar fixed-bottom navbar-light d-block d-lg-none bottom-tab-nav"
+            role="navigation">
+            <Nav className="w-100">
+                <div className=" d-flex flex-row justify-content-around w-100">
+                    {
+                        tabs.map((tab, index) => (
+                            <NavItem key={`tab-${index}`}>
+                                <NavLink to={tab.route} className="nav-link">
+                                    <BottomNavText className="row justify-content-center align-items-center">
+                                        <FontAwesomeIcon size="lg" icon={tab.icon}/>
+                                        <div className='text-center'>{tab.label}</div>
+                                    </BottomNavText>
+                                </NavLink>
+                            </NavItem>
+                        ))
+                    }
+                </div>
+            </Nav>
+        </BottomNavDiv>
+
     );
 }
 
