e924dbc3b6903d8716fea1ebf01cdc4623fbbd20
sorrynthx <sorrynthx@gmail.com>
2022-05-11 00:24:35 +0900

    2022-05-10 티켓마요 프로젝트 내 정보 화면 구성 + 로그인 화면

---
+++ ticketmayo-front/src/components/MyCargo_My.tsx
@@ -0,0 +1,24 @@
+import { Helmet } from "react-helmet-async";
+import styled from "styled-components";
+
+const Header = styled.header`
+    height: 10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+function MyCargo_My() {
+    return (
+        <>
+            <Helmet>
+                <title>내 정보</title>
+            </Helmet>
+            <Header>
+                내 정보
+            </Header>
+        </>
+    );
+};
+
+export default MyCargo_My;
